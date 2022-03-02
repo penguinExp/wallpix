35d8475032b963ffd2d56f207898de4915842811
AdityaMotale <77727680+AdityaMotale@users.noreply.github.com>
2022-03-02 18:48:21 +0530

    Updated packages and created the foundations for the api

--- lib/imgList.view.dart
+++ lib/imgList.view.dart
@@ -1,18 +1,70 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:rive/rive.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:wallpix/core/keys.core.dart';
+import 'package:wallpix/model.dart';
 
 class ImgListView extends StatelessWidget {
-  const ImgListView({Key? key}) : super(key: key);
+  ImgListView({Key? key}) : super(key: key);
+  final List<Img> imgList = <Img>[];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: GridView.count(
-        shrinkWrap: true,
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        crossAxisCount: 2,
-        mainAxisSpacing: 5,
-        crossAxisSpacing: 5,
-      ),
-    );
+        child: FutureBuilder(
+      future: getImgs(imgList: imgList),
+      builder: (context, snapshot) {
+        return snapshot.connectionState == ConnectionState.done
+            ? GridView.count(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                crossAxisCount: 2,
+                // shrinkWrap: true,
+                // crossAxisSpacing: 0.1,
+                mainAxisSpacing: 20,
+                childAspectRatio: MediaQuery.of(context).size.width /
+                    (MediaQuery.of(context).size.height / 1.5),
+                physics: const ClampingScrollPhysics(),
+                children: imgList.map((img) {
+                  return GridTile(
+                    child: GestureDetector(
+                      onTap: (() {}),
+                      child: Hero(
+                        tag: img.id,
+                        child: SizedBox(
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child: FadeInImage.memoryNetwork(
+                                fit: BoxFit.contain,
+                                placeholder: kTransparentImage,
+                                image: img.src.portrait),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              );
+      },
+    ));
+  }
+
+  Future<void> getImgs({required List<Img> imgList}) async {
+    http.Response response = await http.get(
+        Uri.parse("https://api.pexels.com/v1/curated?per_page=80&page=1"),
+        headers: {"Authorization": accessKey});
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    List<dynamic> imgBody = jsonData['photos'];
+    for (var element in imgBody) {
+      imgList.add(Img.fromJson(element));
+    }
   }
 }
--- lib/imgview.dart
+++ lib/imgview.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
+import 'package:wallpix/imgList.view.dart';
 
-import 'model.dart';
 
 class PageUI extends StatefulWidget {
   const PageUI({Key? key}) : super(key: key);
@@ -19,48 +14,9 @@ class _PageState extends State<PageUI> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: GetX(
-            init: Get.put<Imgctrl>(Imgctrl()),
-            builder: (Imgctrl ctrl) {
-              return ctrl.initialized
-                  ? const Center(
-                      child: SizedBox(
-                        height: 200,
-                        width: 200,
-                        // child: Image.network(ctrl.imgList[0].src.portrait),
-                      ),
-                    )
-                  : const Center(
-                      child: CircularProgressIndicator(),
-                    );
-            }),
+        // padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: ImgListView()
       ),
     );
-  }
-}
-
-class Imgctrl extends GetxController {
-  List<Img> imgList = <Img>[];
-
-  Future<void> getImgs() async {
-    http.Response response = await http.get(
-        Uri.parse("https://api.pexels.com/v1/curated?per_page=10&page=1"),
-        headers: {
-          "Authorization":
-              '563492ad6f917000010000012f79d12ef37c4b1cb056cf13e223020a'
-        });
-    Map<String, dynamic> jsonData = jsonDecode(response.body);
-    List<dynamic> imgBody = jsonData['photos'];
-    for (var element in imgBody) {
-      imgList.add(Img.fromJson(element));
-    }
-  }
-
-  @override
-  Future<void> onInit() async {
-    await getImgs();
-    log(imgList.length.toString());
-    super.onInit();
   }
 }
