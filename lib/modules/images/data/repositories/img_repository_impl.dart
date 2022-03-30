269f901f110b24bf14f234dbba5895cb523149c3
Aditya <77727680+AdityaMotale@users.noreply.github.com>
2022-03-30 21:12:03 +0530

    added custom typedef in img repo implemenetation and made some corrections in .gitignore

--- lib/modules/images/data/repositories/img_repository_impl.dart
+++ lib/modules/images/data/repositories/img_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
+import 'package:wallpix/core/errors/exception.core.dart';
 
 import '../../../../core/core.dart';
 import '../../domain/domain.images.dart';
 import '../dataSource/img_data_source.data.dart';
+
+typedef _GetSearchOrCurated = Future<List<ImgEntity>> Function();
 
 class ImgRepositoryImpl implements ImgRepository {
   final ImageDataSource imageDataSource;
@@ -16,16 +19,30 @@ class ImgRepositoryImpl implements ImgRepository {
   Future<Either<Failure, List<ImgEntity>>> getCuratedImgs(
     int page,
   ) async {
-    networkInfo.isConnected;
-    final result = await imageDataSource.getCuratedImgs(page);
-    return Right(result);
+    return await getDataFromSource(
+      () => imageDataSource.getCuratedImgs(page),
+    );
   }
 
   @override
   Future<Either<Failure, List<ImgEntity>>> searchImgs(
       String query, int page) async {
-    networkInfo.isConnected;
-    final result = await imageDataSource.searchImgs(query, page);
-    return Right(result);
+    return await getDataFromSource(
+      () => imageDataSource.searchImgs(query, page),
+    );
+  }
+
+  Future<Either<Failure, List<ImgEntity>>> getDataFromSource(
+      _GetSearchOrCurated getSearchOrCurated) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await getSearchOrCurated();
+        return Right(result);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(ConnectivityFailure());
+    }
   }
 }
